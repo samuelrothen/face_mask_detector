f25e25e501a8b47da9fd40d121710b86685e603c
Samuel Rothen <70198663+samuelrothen@users.noreply.github.com>
2020-11-27 16:29:55 +0100

    Updated Model Training (Mask Reco)

--- src/model_mask_training.py
+++ src/model_mask_training.py
@@ -7,32 +7,8 @@ from sklearn.model_selection import train_test_split
 from tensorflow.keras.applications import mobilenet_v2
 from tensorflow.keras import preprocessing
 
-
-
 classes=['mask','no_mask']
-
 base_path= '../data/'
-
-data_gen = preprocessing.image.ImageDataGenerator(
-    preprocessing_function=mobilenet_v2.preprocess_input,
-    fill_mode='nearest',
-    rotation_range=20,
-    width_shift_range=0.2,
-    height_shift_range=0.2,
-    horizontal_flip=True,
-    zoom_range=0.2,
-    shear_range=0.2
-)
-
-train_data_gen = data_gen.flow_from_directory(
-        directory=base_path,
-        class_mode="categorical",
-        classes=classes,
-        batch_size=100,
-        target_size=(224, 224)
-)
-
-xtrain, ytrain = next(train_data_gen)
 
 
 base_model = mobilenet_v2.MobileNetV2(
@@ -40,12 +16,9 @@ base_model = mobilenet_v2.MobileNetV2(
     alpha=0.35,
     pooling='avg',
     include_top=False,
-    input_shape=(224, 224, 3)
-)
-
+    input_shape=(224, 224, 3))
 
 base_model.trainable = False
-
 
 model = keras.Sequential()
 model.add(base_model)
@@ -59,15 +32,48 @@ model.compile(optimizer=keras.optimizers.Adam(learning_rate=0.001),
               loss=keras.losses.categorical_crossentropy,
               metrics=[keras.metrics.categorical_accuracy])
 
-callback = keras.callbacks.EarlyStopping(monitor='val_loss', patience=3)
+callback = keras.callbacks.EarlyStopping(monitor='loss', patience=3)
 
-model.fit(xtrain, ytrain, 
-          epochs=100, 
+
+
+data_gen = preprocessing.image.ImageDataGenerator(
+    preprocessing_function=mobilenet_v2.preprocess_input,
+    fill_mode='nearest',
+    rotation_range=20,
+    width_shift_range=0.2,
+    height_shift_range=0.2,
+    horizontal_flip=True,
+    zoom_range=0.2,
+    shear_range=0.2,
+    validation_split=0.2)
+
+train_generator = data_gen.flow_from_directory(
+        directory=base_path,
+        class_mode="categorical",
+        classes=classes,
+        batch_size=32,
+        target_size=(224, 224),
+        subset='training')
+
+validation_generator = data_gen.flow_from_directory(
+        directory=base_path,
+        class_mode="categorical",
+        classes=classes,
+        batch_size=32,
+        target_size=(224, 224),
+        subset='validation')
+
+
+
+m_hist=model.fit(train_generator, 
+          epochs=50,
+          # steps_per_epoch=500,
           verbose=2,
-          batch_size=len(xtrain), 
           callbacks=[callback],
-          validation_split=0.3)
+          validation_data=validation_generator,
+          # validation_steps=250
+          )
 
 
 
-model.save('../models/test.h5')
+# model.save('../models/test.h5')
