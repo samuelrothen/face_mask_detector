60afd3f5d7c2c3abedaa8fd99fc5fc774542923c
samuelrothen <samuelrothen@users.noreply.github.com>
2020-12-01 11:05:41 +0000

    autopep8 action fixes

--- misc_tools/batch_rename.py
+++ misc_tools/batch_rename.py
@@ -1,12 +1,12 @@
 import os
 
-#Small tool for batch-renaming Files
+# Small tool for batch-renaming Files
 
-path='../data/no_mask/'
-files=os.listdir(path)
+path = '../data/no_mask/'
+files = os.listdir(path)
 
-for i,file in enumerate(files):
-    file_name,ext=os.path.splitext(file)
-    old_file=f'{path}{file}'
-    new_filename=f'no_mask_{i+1}{ext}'
+for i, file in enumerate(files):
+    file_name, ext = os.path.splitext(file)
+    old_file = f'{path}{file}'
+    new_filename = f'no_mask_{i+1}{ext}'
     os.rename(old_file, new_filename)
